45c311910d0e54a8bac72fa5acf832df02d5873f
petr.kaiser <arowik@gmail.com>
2025-10-29 19:37:35 +0100

    case response desc requirement level

--- Webresources/JavaScripts/CaseResponse.js
+++ Webresources/JavaScripts/CaseResponse.js
@@ -6,6 +6,11 @@
     // Set visibility for ap_description
     var apDescriptionVisible = [0, 1, 2, 3, 5].includes(apType);
     formContext.getControl("ap_description").setVisible(apDescriptionVisible);
+    if (apDescriptionVisible) {
+        formContext.getAttribute("ap_description").setRequiredLevel("required");
+    } else {
+        formContext.getAttribute("ap_description").setRequiredLevel("none");
+    }
 
     // Set visibility for ap_internalcasenumber
     var apInternalCaseNumberVisible = [1, 5].includes(apType);
